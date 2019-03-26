ed9091d050057d03dbe3ca43f0538b11c9b3f599
Manuel Moreno <manuel.moreno@televisatim.com>
2019-03-26 15:56:02 -0600

    Se termin la face de insertar

---
+++ schema/index.js
@@ -0,0 +1,27 @@
+const { makeExecutableSchema } = require('graphql-tools')
+const resolvers = require('../resolvers')
+const Profesor = require('./Profesor')
+const Curso = require('./Curso')
+
+const rootQuery = `
+    type Query {
+        cursos: [Curso]
+        profesores: [Profesor]
+        curso(id: Int): Curso
+        profesor(id: Int): Profesor
+    }
+    
+    type Mutation {
+        profesorAdd(profesor: NuevoProfesor): Profesor
+    }
+  
+`
+
+
+
+const index = makeExecutableSchema({
+    typeDefs: [rootQuery, Profesor, Curso],
+    resolvers
+})
+
+module.exports = index
